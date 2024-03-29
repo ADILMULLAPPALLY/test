d8083608d73326f6de50a79e128fc9bfb59e16fd
AdilKL <adil.m@konceptslab.com>
2024-03-28 15:03:37 -1200

    new project

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+  // Function to show the loader
+  function showLoader() {
+    document.getElementById('loader').style.display = 'flex';
+}
+
+// Function to hide the loader
+function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+}
+
+// Example usage: show loader when the page starts loading
+window.addEventListener('load', function() {
+    showLoader(); // Show loader when the page starts loading
+    // After some delay (e.g., 2 seconds), hide the loader
+    setTimeout(hideLoader, 2000); // Example: Hide loader after 2 seconds
+});
